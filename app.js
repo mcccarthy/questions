f0246d4b3902c2350315e8ea3f5c46c643c89331
James Mccarthy <mccarthyjames35@yahoo.com>
2024-09-03 15:51:06 -0700

    competed the last of the js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-//using selectors inside the element
-// traversing the dom
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		// First, remove 'show-text' class from all questions
+		btns.forEach(function (btn) {
+			const question = btn.parentElement.parentElement;
+			question.classList.remove('show-text');
+		});
+
+		// Then, toggle the 'show-text' class on the clicked question
+		const question = e.currentTarget.parentElement.parentElement;
+		question.classList.toggle('show-text');
+	});
+});
